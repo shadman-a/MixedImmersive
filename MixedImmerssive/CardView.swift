14304b3aa226c8005f22e8a2d52fe08aa5633ef3
shadman-a <ahmedshadman966@gmail.com>
2023-10-24 12:47:14 -0400

    Created tabBar cards and home screen

---
+++ MixedImmerssive/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  MixedImmerssive
+//
+//  Created by Shadman Ahmed on 10/24/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
